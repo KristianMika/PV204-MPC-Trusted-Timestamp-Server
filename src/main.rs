2414089062183742e8cdb19ae978721402be2c19
PrisionMike <su.sh2396@gmail.com>
2022-03-20 16:53:56 +0100

    getting to first round

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use frost_dalek::{Parameters, Participant};
+use frost_dalek::{Parameters, Participant, DistributedKeyGeneration};
 
-fn main() {
+/*
+Comments are mostly summary / comments from the docs.
+*/
+fn main() -> std::io::Result<()> {
     println!("Czech or Slovakia?");
     let params = Parameters { t:2, n:3 };
 
@@ -12,6 +15,47 @@ fn main() {
     These participant indices need to be agree-ed upon out of scope. We could use our UCO's here. like ID numbers.
     As we can also make them public. THat edit will be tried later.
     This also helps saving an exchange step.
+    
+    - Okay these structs by our names need to be shared : david, kritian..
+    The corresponding coeffs are private.
     */
+
+    // Each of us need to verify the other 2 person's zkp by doing this:
+    /* EDIT IN YOUR SERVERS */
+    let daveproof = match david.proof_of_secret_key.verify(&david.index, &david.public_key().unwrap()) {
+        Ok(v) => v,
+        Err(e) => panic!("Not David! NOT DAVID")
+    };
+
+    
+
+    let krisproof = match kristian.proof_of_secret_key.verify(&kristian.index, &kristian.public_key().unwrap()) {
+        Ok(v) => v,
+        Err(e) => panic!("Not Kristian! NOT KRISTIAN")
+    };
+
+    // Suyash enters round one of the distributed key exchange
+    let mut suyash_other_parts: Vec<Participant> = vec![david.clone(),kristian.clone()];
+
+    // I seemingly verify your zkp's again. This time for the purpose of Dist. key gen.
+    let suyash_state = DistributedKeyGeneration::<_>::new(&params, &suyash.index, &suyash_coef, &mut suyash_other_parts).unwrap();
+
+    // This is a secret share step. Needs to be done with auth/enc etc. This is what I will share with other participants.
+    let suyash_their_secret_shares = suyash_state.their_secret_shares();
+
+    /*
+    To be done later:
+    send_to_david(suyash_their_secret_shares)
+    send_to_kristian(suyash_their_secret_shares)
+    
+    .. David and Kristian have to do the same.
+
+    I am gonna create your secret shares here, but in reality I am having these
+    sent from you: (Remove during produciton)
+    */
+
+    let mut david_other_parts: Vec<Participant> = vec![suyash.clone(), kristian.clone()];
+
+    Ok(())
 
 }
