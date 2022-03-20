69a4334d89d71e34054753b39c2a66f468bbc61c
PrisionMike <su.sh2396@gmail.com>
2022-03-20 13:35:33 +0100

    params and participants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use frost_dalek::{Parameters, Participant};
+
 fn main() {
     println!("Czech or Slovakia?");
+    let params = Parameters { t:2, n:3 };
+
+    let (david, david_coef) = Participant::new(&params, 1); // The 1 here is called the _participant index_
+    let (kristian, kritian_coef) = Participant::new(&params, 2);
+    let (suyash, suyash_coef) = Participant::new(&params, 3);
+
+    /*
+    These participant indices need to be agree-ed upon out of scope. We could use our UCO's here. like ID numbers.
+    As we can also make them public. THat edit will be tried later.
+    This also helps saving an exchange step.
+    */
+
 }
