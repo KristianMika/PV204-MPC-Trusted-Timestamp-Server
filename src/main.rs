e219351e803a91c6068c2fe0efcb46595ea65f8d
PrisionMike <su.sh2396@gmail.com>
2022-03-22 09:41:29 +0100

    hash stamp and signing done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// use mpc_frost_dalek::*;
 use frost_dalek::{Parameters,
                 Participant,
                 DistributedKeyGeneration,
@@ -6,17 +7,26 @@ use frost_dalek::{Parameters,
                 SignatureAggregator};
 
 use rand::rngs::OsRng;
-
+use std::time::SystemTime;
+use chrono::offset::Utc;
+use chrono::DateTime;
+use sha2::{Digest, Sha256};
 /*
 Comments are mostly summary / comments from the docs.
 */
+
+//Implemented in another branch. Copying here for demonstration.
+pub struct TimeStampResp {
+    status: String,         // for now, later PKIStatusInfo
+    timeStampToken: String, // TODO: read the RFC 3161
+}
+
 fn main() {
     let params = Parameters { t:2, n:3 };
 
     let (david, david_coef) = Participant::new(&params, 1);
     let (kristian, kristian_coef) = Participant::new(&params, 2);
     let (suyash, suyash_coef) = Participant::new(&params, 3);
-
     /*
     These participant indices need to be agree-ed upon out of scope. I did it alphabetically. But it also makes for a cool abbreviation: DIKS.
     Which stands for DIstributed Key Signing.
@@ -97,11 +107,13 @@ fn main() {
     let david_public_key = david_secret_key.to_public();
     let kristian_public_key = kristian_secret_key.to_public();
 
-    println!("Suyash's public key: {:?}", suyash_public_key);
-    println!("Davi's public key: {:?}", david_public_key);
-    println!("Kristi's public key: {:?}", kristian_public_key);
+    // println!("Suyash's public key: {:?}", suyash_public_key);
+    // println!("Davi's public key: {:?}", david_public_key);
+    // println!("Kristi's public key: {:?}", kristian_public_key);
 
     /*========= KEY ESTABLISHMENT OVER ============= */
+
+
     /*========= Signing ============= */
 
     let (ash_public_comshares, mut ash_secret_comshares) = generate_commitment_share_lists(&mut OsRng, 3, 1);
@@ -110,13 +122,33 @@ fn main() {
 
     /* CONTEXT = A byte string, kinda public, pertinent to this application. So this will be a constant for the group. */
     const CONTEXT: &[u8] = b"PV204_PETR_SVENDA_ANTONIN_DUFKA";
-    let message = b"MV013 sucks. I don't wanna study this course";
 
-    let message_hash = compute_message_hash(&CONTEXT[..], &message[..]);
+    // - File hash received by the user.
+    let file_hash = b"HASH-OF-THE-FILE---RECEIVED-BY-THE-USER--PRELIM-TESTED-BY-US";
+    
+    //Timestamp obtained at the instant of received file.
+    let timenow = SystemTime::now();
+    let datetime: DateTime<Utc> = timenow.into();
+    let timestr = datetime.format("%Y%m%d%H%M%SZ").to_string();
+    println!("RFC 3161 compliant timestamp: {}",timestr);
+    let timestr = timestr.as_bytes();      // Time Stamp in UTC to avoid timezone issues. Format compliant with RFC 3161
+
+    let mut hasher = Sha256::new();
+    hasher.update(file_hash);
+    hasher.update(timestr);
+    let fin_hash = hasher.finalize();
+
+
+
+
+
+
+
+    let message_hash = compute_message_hash(&CONTEXT[..], &fin_hash[..]);
 
     /* This aggregator will assign signers, pull in their signatures, and finalise theri sign.
     The best part is, we don't trust the aggregator. THE AGGREGATOR IS UNTRUSTED. It could be our user, one of us, a standalone app, Dufka, Jennifer Lawrence, anyone we want.*/
-    let mut aggregator = SignatureAggregator::new(params, suyash_group_key.clone(), &CONTEXT[..], &message[..]);
+    let mut aggregator = SignatureAggregator::new(params, suyash_group_key.clone(), &CONTEXT[..], &fin_hash[..]);
 
     /*Aggregator nominates Kristian and I */
     // Don't know where the 0 index in the published_commitment_share is coming from. Right now.
@@ -152,5 +184,7 @@ fn main() {
         Err(e) => panic!("Bad signing. Likely corrupted signees or signatures!\n{:?}",e)
     };
 
-    println!("The message: {:?}\nThe Context: {:?}\nThe Signature: {:?}", &message, &CONTEXT, threshold_sign);
+    println!("The message: {:?}\nThe Context: {:?}\nThe Timestamp: {:?}\nThe Signature: {:?}", &fin_hash, &CONTEXT,  timestr, threshold_sign);
+
+
 }
