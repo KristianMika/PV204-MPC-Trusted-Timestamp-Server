dc545a1463dcbe7881e31d060b794bc5ea268b12
Kristian Mika <kristian.mika@protonmail.com>
2022-04-09 23:56:10 +0200

    feat: use a config file
    - read a server index from env

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,55 @@ use timestamp_server::ServerState;
 mod services;
 use services::*;
 use std::env;
+use serde::{Deserialize};
 
+
+#[derive(Deserialize)]
+struct Config {
+    servers: Vec<String>
+}
+
+const CONFIG_PATH:&str = "config/config.toml"; 
+fn read_config() -> std::io::Result<Config> {
+    let content = std::fs::read_to_string(CONFIG_PATH)?;
+    Ok(toml::from_str(&content)?)
+}
+
+// TODO: consider anyhow
+// TODO: use an automata and track states + check transitions
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // TODO: use an automata and track states + check transitions
+    env_logger::init();
+    let server_index: String = match env::var("SERVER_INDEX") {
+        Ok(val) => val,
+        Err(_) => panic!("SERVER_INDEX is not set.")
+    };
+    let server_index: i32 = match server_index.parse(){
+        Ok(val) => val,
+        Err(_) => panic!("Invalid SERVER_INDEX value: {}", server_index)
+    };
+    log::info!(
+        "Starting server with index {}.", server_index
+    );
 
-    // TODO: error handling!
+    let config: Config = match read_config() {
+        Ok(val) => val,
+        Err(_) => panic!("Could not read/parse the config file at path {}.", CONFIG_PATH)
+    };
+
+    log::info!(
+        "Successfully loaded the config file."
+    );
+
     // TODO: load from env variables / a config file
     let argv: Vec<String> = env::args().collect();
 
-    if argv.len() != 5 {
-        return Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Current tmp usage: cargo run [server_id] [server_port] [ip2:port2] [ip3:port3]",
-        ));
-    }
+    // if argv.len() != 5 {
+    //     return Err(std::io::Error::new(
+    //         std::io::ErrorKind::Other,
+    //         "Current tmp usage: cargo run [server_id] [server_port] [ip2:port2] [ip3:port3]",
+    //     ));
+    // }
 
     // TODO: a temporary CLI arguments handling
     let default_params = Parameters { t: 2, n: 3 };
