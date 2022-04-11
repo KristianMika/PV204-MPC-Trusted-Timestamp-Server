16e9f481a98556d993198fe00217fe914a1c6b04
Kristian Mika <kristian.mika@protonmail.com>
2022-04-12 01:52:50 +0200

    chore: remove containers after the binary has been  built

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 set -e
 
 cargo fmt
+docker run --rm -it -v $(pwd):/home/rust/src ekidd/rust-musl-builder cargo build
 docker rmi --force diks-tits || 1
 docker-compose rm --stop -v -f || 1
-docker run --rm -it -v $(pwd):/home/rust/src ekidd/rust-musl-builder cargo build
-
 docker build . --tag diks-tits:latest
