8abafa8dd5655cd3a4a7f3e0eac6e735149ba7f8
davedr <david.184@seznam.cz>
2022-04-12 09:39:40 +0200

    Does not work

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ mod services;
 use services::*;
 use std::env;
 
+use rustls::{Certificate, PrivateKey};
 use rustls::server::{ServerConfig};
 use rustls::server::AllowAnyAnonymousOrAuthenticatedClient;
 use rustls_pemfile::{certs, read_all, pkcs8_private_keys};
 
-use std::io::BufReader;
+use std::io::{BufReader, Read};
 use std::fs::File;
 
 
@@ -67,21 +68,55 @@ async fn main() -> std::io::Result<()> {
     let key_file = &mut BufReader::new(
     File::open("certificates/privkey.pem").unwrap());
 
+    let der_cert_file =std::fs::read("certificates/certs.der")?;
+
     // Parse the certificate and set it in the configuration
     // may show error of self declared cerificate or something like that
     let cert_chain = read_all(cert_file).unwrap();
-    //this is in case of der fromat which should probably be used
+    //certificates
     let der_certs = certs(cert_file).unwrap();
+    // keys
     let mut keys = pkcs8_private_keys(key_file).unwrap();
+    // use the first key
+    let mut key = PrivateKey(keys[0]);
+
+    let mut certificate_certs : Vec<Certificate>;
+
+    for cert in der_certs {
+        certificate_certs.push(Certificate(cert));
+    }
+
 
     // empty for now
     let mut roots = rustls::RootCertStore::empty();
+    roots.add(&Certificate(der_cert_file));
 
     let mut config = ServerConfig::builder()
                     .with_safe_defaults()
                     //how the fuck do I create RootCertStore
                     .with_client_cert_verifier(AllowAnyAnonymousOrAuthenticatedClient::new(roots))
-                    .with_single_cert(der_certs, key).unwrap();
+                    .with_single_cert(certificate_certs, key).unwrap();
+    
+    //let certfile = std::fs::read("./tits-with-diks/ourCA2.der")?;
+    // root_store.add(&Certificate(certfile)).unwrap();
+
+    // let config = rustls::ClientConfig::builder()
+    //             .with_safe_defaults()
+    //             .with_root_certificates(root_store);
+
+
+    // what will client do, maybe?
+    let mut buf = Vec::new();
+    File::open("my-ident.pem")?.read_to_end(&mut buf)?;
+    
+    let mut cer_buf = Vec::new();
+    File::open("my-ident.pem")?.read_to_end(&mut cer_buf)?;
+
+    let pkcs12 = reqwest::Identity::from_pem(&buf).unwrap();
+    let client = reqwest::Client::builder()
+    .identity(pkcs12)
+    .build().unwrap();
+
 
 
     log::info!("Starting the server at {}.", server_address);
